f5e516f28a9c4538b90a9274b9baa57860828cfb
keksiktusik <martaprzyszowa@gmail.com>
2024-10-16 13:49:24 +0200

    faktyczne zaczecie kodu

--- index.cmd
+++ index.cmd
@@ -1,12 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class DoublyLinkedList {
+private:
+    struct Node {
+        int data;
+        Node* next;
+        Node* prev;
+        Node(int value) : data(value), next(nullptr), prev(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    // Dodanie elementu na początek listy
+    void addToStart(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
 int main(){
     int commit;
     cout<<"pierwszy commit"<<endl;
     cin>>commit;
-
-    //test
-    return 0;
+              return 0;
 
 }
